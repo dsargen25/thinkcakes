f7017c339b3c897e298c0233aa6aeb46f7013300
Alex Varela <alexvar93@gmail.com>
2020-10-01 21:40:36 -0400

    fixed api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
-// const cakesdb = require('../models/cakes.js');
 
 module.exports = (passport, db) => {
   const AuthController = require('../controllers/authController')(passport, db);
@@ -22,61 +21,61 @@ module.exports = (passport, db) => {
   return router;
 };
 
-// Cake controller
-// module.exports = (cakesdb) => {
-//   const CakeController = require('../controllers/cakeController')(cakesdb);
-//   // Cake Routes
-//   router.get('/api/cakes', function (req, res) {
-//     CakeController.getAllCakes(function (result) {
-//       console.log({ Cakes: result });
-//     });
-//   });
-//   router.get('/api/cakes/:id', function (req, res) {
-//     CakeController.getSpecificCakes(function (result) {
-//       console.log({ Cakes: result });
-//     });
-//   });
-//   router.get('/api/cakes/:id', function (req, res) {
-//     CakeController.getUserCakes(function (result) {
-//       console.log({ Cakes: result });
-//     });
-//   });
-//   router.post('/api/cakes', function (req, res) {
-//     CakeController.createNewCake(function (result) {
-//       console.log({ Cakes: result });
-//       console.log('New cake has been added to the database!');
-//     });
-//   });
-//   router.delete('/api/cakes/:id', function (req, res) {
-//     CakeController.deleteCakePost(function () {
-//       console.log('Cake entry has been deleted!');
-//     });
-//   });
+module.exports = cakesdb => {
+  const CakeController = require('../controllers/cakeController');
+  // Cake Routes
+  router.get('/api/cakes', function (req, res) {
+    CakeController.getAllCakes(function (result) {
+      console.log({ Cakes: result });
+    });
+  });
+  router.get('/api/cakes/:id', function (req, res) {
+    CakeController.getSpecificCakes(function (result) {
+      console.log({ Cakes: result });
+    });
+  });
+  router.get('/api/cakes/:id', function (req, res) {
+    CakeController.getUserCakes(function (result) {
+      console.log({ Cakes: result });
+    });
+  });
+  router.post('/api/cakes', function (req, res) {
+    CakeController.createNewCake(function (result) {
+      console.log({ Cakes: result });
+      console.log('New cake has been added to the database!');
+    });
+  });
+  router.delete('/api/cakes/:id', function (req, res) {
+    CakeController.deleteCakePost(function () {
+      console.log('Cake entry has been deleted!');
+    });
+  });
 
-//   // Comment Routes
-//   router.get('/api/comments', function (req, res) {
-//     CakeController.getCakeComments(function (result) {
-//       console.log({ Comments: result });
-//     });
-//   });
-//   router.get('/api/comments/:id', function (req, res) {
-//     CakeController.getUserComments(function (result) {
-//       console.log({ Comments: result });
-//     });
-//   });
-//   router.post('/api/comments', function (req, res) {
-//     CakeController.createNewComment(function (result) {
-//       console.log({ Comments: result });
-//       console.log('New comment has been added!');
-//     });
-//   });
-//   router.delete('/api/comments/:id', function (req, res) {
-//     CakeController.deleteComment(function () {
-//       console.log('Comment has been deleted!');
-//     });
-//   });
-// };
+  // Comment Routes
+  router.get('/api/comments', function (req, res) {
+    CakeController.getCakeComments(function (result) {
+      console.log({ Comments: result });
+    });
+  });
+  router.get('/api/comments/:id', function (req, res) {
+    CakeController.getUserComments(function (result) {
+      console.log({ Comments: result });
+    });
+  });
+  router.post('/api/comments', function (req, res) {
+    CakeController.createNewComment(function (result) {
+      console.log({ Comments: result });
+      console.log('New comment has been added!');
+    });
+  });
+  router.delete('/api/comments/:id', function (req, res) {
+    CakeController.deleteComment(function () {
+      console.log('Comment has been deleted!');
+    });
+  });
 
+  return router;
+};
 // router.get('/api/cakes', CakeController.getAllCakes);
 // router.get('/api/cakes/:id', CakeController.getSpecificCakes);
 // router.get('/api/cakes/:id', CakeController.getUserCakes);
